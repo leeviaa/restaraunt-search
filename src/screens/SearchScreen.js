54a348e6148e04acb6c2e6884678931fcfcc26e4
leeviaa <leeviaa22@gmail.com>
2020-11-09 18:13:17 -0500

    Updated styling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import useResults from '../hooks/useResults'
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
-
+  
+  //filter results func
+  const filterResultsByPrice = (price) => {
+    return results.filter( result => {
+      return result.price === price
+    })
+  }
   return (
     <View>
        {  errorMessage ? <Text>{errorMessage}</Text>: null}
@@ -16,6 +23,9 @@ const SearchScreen = () => {
        onTermSubmit={() => searchApi(term)}
       />
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
   );
 };
